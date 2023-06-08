f8e2c1dab2bb704bc2c2885cf3d7bdc85c9f688d
junaidmuiz4@gmail.com <106062220+junaidmuiz@users.noreply.github.com>
2023-06-08 17:08:36 +0100

    recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+*_puts_recursion - duplicate of puts();
+*@s: input string
+*Return: 0 SUCCESS
+*/
+void _puts_recursion(char *s)
+{int con = 0;
+
+recursion_ass(s, con);
+}
+
+/*
+ *recursion_ass - puts_recursion assistant
+ * @s: inputing string
+ * @con: index position
+*/
+void recursion_ass(char *s, int con)
+{
+	int len = strlen(s);
+
+	if (con < len)
+	{
+		_putchar(s[con]);
+		con++;
+		recursion_ass(s, con);
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
